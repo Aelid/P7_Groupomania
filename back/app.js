fdb3bd6c30d794e83b904c1bb775f68407a8377b
Aelid <85112285+Aelid@users.noreply.github.com>
2022-07-25 10:36:47 +0200

    Add files via upload

---
+++ back/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const articlesRoutes = require("./routes/articles");
+const userRoutes = require("./routes/user");
+
+mongoose
+  .connect(
+    "mongodb+srv://Aelid:YYOP9fTBNdZbk7HV@cluster0.x1h4xr0.mongodb.net/Groupomania",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use("/api/articles", articlesRoutes);
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
